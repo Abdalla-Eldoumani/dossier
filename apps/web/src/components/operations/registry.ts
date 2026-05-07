f44d65c49115c1e40e05ae0390001b4a9c467e6c
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-05-07 16:57:34 -0600

    register all operation views

--- apps/web/src/components/operations/registry.ts
+++ apps/web/src/components/operations/registry.ts
@@ -18,6 +18,31 @@ import { PdfToTextOperation } from "./PdfToTextOperation";
 import { PdfToMarkdownOperation } from "./PdfToMarkdownOperation";
 import { CompressOperation } from "./CompressOperation";
 import { MergeOperation } from "./MergeOperation";
+import { LineariseOperation } from "./LineariseOperation";
+import { SubsetFontsOperation } from "./SubsetFontsOperation";
+import { RepairOperation } from "./RepairOperation";
+import { DownsampleImagesOperation } from "./DownsampleImagesOperation";
+import { OcrOperation } from "./OcrOperation";
+import { DecryptOperation } from "./DecryptOperation";
+import { EncryptOperation } from "./EncryptOperation";
+import { SetPermissionsOperation } from "./SetPermissionsOperation";
+import { WatermarkOperation } from "./WatermarkOperation";
+import { PageNumbersOperation } from "./PageNumbersOperation";
+import { HeaderFooterOperation } from "./HeaderFooterOperation";
+import { CropPagesOperation } from "./CropPagesOperation";
+import { ResizePagesOperation } from "./ResizePagesOperation";
+import { InsertBlankOperation } from "./InsertBlankOperation";
+import { InsertFromPdfOperation } from "./InsertFromPdfOperation";
+import { AddTextAnnotationOperation } from "./AddTextAnnotationOperation";
+import { HighlightOperation } from "./HighlightOperation";
+import { StampOperation } from "./StampOperation";
+import { RedactOperation } from "./RedactOperation";
+import { PdfToImagesOperation } from "./PdfToImagesOperation";
+import { ImagesToPdfOperation } from "./ImagesToPdfOperation";
+import { HtmlToPdfOperation } from "./HtmlToPdfOperation";
+import { FillFormOperation } from "./FillFormOperation";
+import { SplitOperation } from "./SplitOperation";
+import { ReorderPagesOperation } from "./ReorderPagesOperation";
 
 export const OPERATION_VIEWS: Record<string, ComponentType> = {
   info: InfoOperation,
@@ -35,4 +60,29 @@ export const OPERATION_VIEWS: Record<string, ComponentType> = {
   "to-markdown": PdfToMarkdownOperation,
   compress: CompressOperation,
   merge: MergeOperation,
+  linearise: LineariseOperation,
+  "subset-fonts": SubsetFontsOperation,
+  repair: RepairOperation,
+  downsample: DownsampleImagesOperation,
+  ocr: OcrOperation,
+  decrypt: DecryptOperation,
+  encrypt: EncryptOperation,
+  permissions: SetPermissionsOperation,
+  watermark: WatermarkOperation,
+  "page-numbers": PageNumbersOperation,
+  "header-footer": HeaderFooterOperation,
+  crop: CropPagesOperation,
+  resize: ResizePagesOperation,
+  "insert-blank": InsertBlankOperation,
+  "insert-from": InsertFromPdfOperation,
+  "add-text": AddTextAnnotationOperation,
+  highlight: HighlightOperation,
+  stamp: StampOperation,
+  redact: RedactOperation,
+  "to-images": PdfToImagesOperation,
+  "from-images": ImagesToPdfOperation,
+  "html-to-pdf": HtmlToPdfOperation,
+  "fill-form": FillFormOperation,
+  split: SplitOperation,
+  reorder: ReorderPagesOperation,
 };
